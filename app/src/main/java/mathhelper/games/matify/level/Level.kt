b074be31a44ec8801f262c19beeb1c4c3030c114
vkatsman <vikto9494@gmail.com>
2021-03-09 19:14:46 +0300

    combinatorics-1 game added

--- app/src/main/java/mathhelper/games/matify/level/Level.kt
+++ app/src/main/java/mathhelper/games/matify/level/Level.kt
@@ -154,7 +154,7 @@ class Level {
         startExpression = structureStringToExpression(startExpressionStr)
         endExpressionStr = levelJson.getString(LevelField.FINAL_EXPRESSION.str)
         endExpression = structureStringToExpression(endExpressionStr)
-        Log.d("task", "${startExpression.toTexView()} -> ${endExpression.toTexView()}")
+        Log.d("task", "'${startExpression} -> ${endExpression}' parsed from '${startExpressionStr} -> ${endExpressionStr}'")
         endPatternStr = levelJson.optString(LevelField.FINAL_PATTERN.str, "")
         endPattern = when (type) {
             Type.SET -> stringToExpressionStructurePattern(endPatternStr, type.str)
